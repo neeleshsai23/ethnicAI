7562e1f58276eedfbcb8388de4c29a9f9e917be4
Neelesh Sai <rneeleshsai@gmail.com>
2020-11-29 13:05:49 +0530

    commit by neelesh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,7 +53,7 @@ import {
 import { ShowroomComponent } from './showroom/showroom.component';
 import { Login2Component } from './login/login2/login2.component';
 import { TestComponent } from './test/test.component';
-import { InventoryComponent } from "./vendor/products/inventory/inventory.component";
+import { CoalescingComponentFactoryResolver } from "./coalescing-component-factory-resolver.service";
 
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
@@ -135,7 +135,8 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     {
       provide: PERFECT_SCROLLBAR_CONFIG,
       useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG,
-    }
+    },
+    CoalescingComponentFactoryResolver
   ],
   bootstrap: [AppComponent],
 })
